40155d9db4a25d59d342869885d7f75406165540
John Koehler <john.koehler@weissmaler.de>
2019-05-11 15:18:28 +0200

    Wrap routes in class

--- server.rb
+++ server.rb
@@ -5,29 +5,32 @@ require './factors.rb'
 require 'bundler'
 Bundler.require
 
-get '/index' do
-  erb :index
-end
+# defines the servers' available routes
+class Server < Sinatra :Base
+  get '/index' do
+    erb :index
+  end
 
-post '/index' do
-  person_params = {
-    gender: params[:gender],
-    age_in_years: params[:age_in_years].to_f,
-    height_in_cm: params[:height_in_cm].to_f,
-    weight_in_kilograms: params[:weight_in_kilograms].to_f
-  }
+  post '/index' do
+    person_params = {
+      gender: params[:gender],
+      age_in_years: params[:age_in_years].to_f,
+      height_in_cm: params[:height_in_cm].to_f,
+      weight_in_kilograms: params[:weight_in_kilograms].to_f
+    }
 
-  activity_params = {
-    hours_sleeping: params[:hours_sleeping].to_f,
-    hours_sitting_low: params[:hours_sitting_low].to_f,
-    hours_sitting_middle: params[:hours_sitting_middle].to_f,
-    hours_sitting_high: params[:hours_sitting_high].to_f,
-    hours_standing: params[:hours_standing].to_f,
-    hours_exerting: params[:hours_exerting].to_f
-  }
+    activity_params = {
+      hours_sleeping: params[:hours_sleeping].to_f,
+      hours_sitting_low: params[:hours_sitting_low].to_f,
+      hours_sitting_middle: params[:hours_sitting_middle].to_f,
+      hours_sitting_high: params[:hours_sitting_high].to_f,
+      hours_standing: params[:hours_standing].to_f,
+      hours_exerting: params[:hours_exerting].to_f
+    }
 
-  calculator = CalculateCaloricMetabolism.new(person_params)
-  @result = calculator.call(activity_params)
+    calculator = CalculateCaloricMetabolism.new(person_params)
+    @result = calculator.call(activity_params)
 
-  erb :result
+    erb :result
+  end
 end
